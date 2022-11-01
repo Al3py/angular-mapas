24aca912cd95e0d9c373b4112a4f895d4b8f10ca
Ale Centurion <al3.cen@hotmail.com>
2022-10-31 21:37:27 -0300

    Fin Seccion 21

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiYWxlcHkiLCJhIjoiY2w5c2tuMjR4MDJ4NjNvcGdwMzE0ZmlwayJ9.iyx7KUudKSejrYSTlTZQ7w'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiYWxlcHkiLCJhIjoiY2w5c2tuMjR4MDJ4NjNvcGdwMzE0ZmlwayJ9.iyx7KUudKSejrYSTlTZQ7w'
 };
 
 /*
